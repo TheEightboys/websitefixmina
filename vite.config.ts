e4bf0181147284c874fed58f3868c3390807e10b
yogiramsuratkumarbhajans-netizen <yogiramsuratkumarbhajans@gmail.com>
2026-02-01 14:39:57 +0530

    Temporarily remove unplugin-auto-import to fix Vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'node:path'
-import AutoImport from 'unplugin-auto-import/dist/vite.mjs'
 
 const base = process.env.BASE_PATH || '/'
 const isPreview = process.env.IS_PREVIEW  ? true : false;
@@ -11,69 +10,11 @@ export default defineConfig({
    __BASE_PATH__: JSON.stringify(base),
    __IS_PREVIEW__: JSON.stringify(isPreview)
   },
-  plugins: [react(),
-    AutoImport({
-      imports: [
-        {
-          'react': [
-            'React',
-            'useState',
-            'useEffect',
-            'useContext',
-            'useReducer',
-            'useCallback',
-            'useMemo',
-            'useRef',
-            'useImperativeHandle',
-            'useLayoutEffect',
-            'useDebugValue',
-            'useDeferredValue',
-            'useId',
-            'useInsertionEffect',
-            'useSyncExternalStore',
-            'useTransition',
-            'startTransition',
-            'lazy',
-            'memo',
-            'forwardRef',
-            'createContext',
-            'createElement',
-            'cloneElement',
-            'isValidElement'
-          ]
-        },
-        {
-          'react-router-dom': [
-            'useNavigate',
-            'useLocation',
-            'useParams',
-            'useSearchParams',
-            'Link',
-            'NavLink',
-            'Navigate',
-            'Outlet'
-          ]
-        },
-        // React i18n
-        {
-          'react-i18next': [
-            'useTranslation',
-            'Trans'
-          ]
-        }
-      ],
-      dts: true,
-    }),
-  ],
+  plugins: [react()],
   base,
   build: {
     sourcemap: true,
     outDir: 'dist',
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
   },
   server: {
     port: 3000,
